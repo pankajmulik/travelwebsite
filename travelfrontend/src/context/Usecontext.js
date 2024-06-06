7b0bf73956bebc375cc339415e4d183373c3f77a
PANKAJ MULIK <pankajmulik1997@gmail.com>
2024-06-06 18:51:35 +0530

    Update Usecontext.js
    Auth contentx

--- travelfrontend/src/context/Usecontext.js
+++ travelfrontend/src/context/Usecontext.js
@@ -3,3 +3,40 @@ import React from 'react'
 import {createContext, useReducer} from 'react'
 
 const usecontext=createContext()
+export const authReducer = (state, action) => {
+    
+
+    switch (action.type) {
+        case "LOGIN":
+            
+            return{user:action.payload }
+    
+        case "LOGOUT":
+
+            return { user:null }
+
+        
+        
+        default:
+           return state;
+    }
+
+}
+
+
+export const AuthContextProvider = ({ children }) => {
+ 
+    const [state, dispatch] = useReducer(authReducer, {
+        user:null
+    })
+
+
+    console.log("AuthContext state", state)
+
+    return (
+        <AuthContext.Provider value={{ ...state, dispatch }}>
+            {children}
+        </AuthContext.Provider>
+    )
+    
+}
