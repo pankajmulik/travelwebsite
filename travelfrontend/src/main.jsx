5c6a7b2c54bba3f427e4d61511a56525b5332b9b
PANKAJ MULIK <pankajmulik1997@gmail.com>
2024-06-08 14:44:49 +0530

    Update main.jsx
    authprovider import

--- travelfrontend/src/main.jsx
+++ travelfrontend/src/main.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {} from './context/Usecontext.js'
+import {AuthContextProvider} from './context/Usecontext.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    
+    <AuthContextProvider>
     <App />
-
+</AuthContextProvider>
     
   </React.StrictMode>,
 )
